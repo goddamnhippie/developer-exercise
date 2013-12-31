f9a2a1c4dfb34965ee2c7711cfea7c55dc86c255
Gustavo Beathyate <gustavo@epiclabs.pe>
2013-12-30 20:46:01 -0500

    remove whitespace

--- ruby/where/test.rb
+++ ruby/where/test.rb
@@ -6,22 +6,22 @@ class WhereTest < Test::Unit::TestCase
     @charles = {:name => 'Charles De Mar', :quote => 'Go that way, really fast. If something gets in your way, turn.', :title => 'Better Off Dead', :rank => 3}
     @wolf    = {:name => 'The Wolf', :quote => 'I think fast, I talk fast and I need you guys to act fast if you wanna get out of this', :title => 'Pulp Fiction', :rank => 4}
     @glen    = {:name => 'Glengarry Glen Ross', :quote => "Put. That coffee. Down. Coffee is for closers only.",  :title => "Blake", :rank => 5}
-    
+
     @fixtures = [@boris, @charles, @wolf, @glen]
   end
-  
+
   def test_where_with_exact_match
     assert_equal [@wolf], @fixtures.where(:name => 'The Wolf'),
   end
-  
+
   def test_where_with_partial_match
     assert_equal [@charles, @glen], @fixtures.where(:title => /^B.*/)
   end
-  
+
   def test_where_with_mutliple_exact_results
     assert_equal [@boris, @wolf], @fixtures.where(:rank => 4)
   end
-  
+
   def test_with_with_multiple_criteria
     assert_equal [@wolf], @fixtures.where(:rank => 4, :quote => /get/)
   end
