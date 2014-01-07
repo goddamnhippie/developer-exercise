a3b3ff30d7e66fc192af2c94298fea8a10f5dcde
Gustavo Beathyate <gustavo@epiclabs.pe>
2014-01-07 18:09:42 -0500

    add accordion tests

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   module("Accordion tests");
 
-  test("example test", function() {
-    ok(true);
+  test("Usage test", function() {
+    ok($('.accordion-header > div').first().is(':visible'));
+
+    var last_link = $('.accordion-header > a').last();
+
+    last_link.click();
+
+    ok(last_link.next('div').is(':visible'));
   });
 });
