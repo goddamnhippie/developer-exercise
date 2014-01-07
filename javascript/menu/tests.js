ca4b62e16af375ccd9183cd2d5cb76db0beca211
Gustavo Beathyate <gustavo@epiclabs.pe>
2014-01-07 18:00:05 -0500

    add menu tests

--- javascript/menu/tests.js
+++ javascript/menu/tests.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
   module("Menu tests");
 
-  test("example test", function() {
-    ok(true);
-  });
+  test("Navigation test", function() {
+    $('#menu').children('a').first().click();
 
+    equal($('#menu').children('a').first().html(), "Back");
+
+    $('#menu').children('a').first().click();
+
+    $('#menu').children('a').first().click();
+
+    equal($('#menu').children('a').last().html(), nestedMenu["study"]["contents"]["notes"]);
+  });
 });
